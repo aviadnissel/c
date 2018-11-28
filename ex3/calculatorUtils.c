6aaaee6ee4d7c0037982e117b8786ce9cb813771
Aviad Nissel <aviadnissel@cs.huji.ac.il>
2018-11-28 14:14:32 +0200

    spaces to tabs

--- ex3/calculatorUtils.c
+++ ex3/calculatorUtils.c
@@ -2,83 +2,83 @@
 
 struct Input popInput(struct Stack* stack)
 {
-    struct Input input;
-    pop(stack, &input);
-    return input;
+	struct Input input;
+	pop(stack, &input);
+	return input;
 }
 
 void pushInput(struct Stack* stack, struct Input input)
 {
-    push(stack, &input);
+	push(stack, &input);
 }
 
 struct Input peekInput(Stack* stack)
 {
-    struct Input headData;
-    headData = popInput(stack);
-    pushInput(stack, headData);
-    return headData;
+	struct Input headData;
+	headData = popInput(stack);
+	pushInput(stack, headData);
+	return headData;
 }
 
 int isChar(struct Input input)
 {
-    return input.type == CHAR_TYPE;
+	return input.type == CHAR_TYPE;
 }
 
 int isOperator(struct Input input)
 {
-    char c;
-    if (!isChar(input))
-    {
-        return 0;
-    }
-    c = (char) input.value;
-    return c == '^' || c == '+' || c == '-' || c == '*' || c == '/'; // TODO consts
+	char c;
+	if (!isChar(input))
+	{
+		return 0;
+	}
+	c = (char) input.value;
+	return c == '^' || c == '+' || c == '-' || c == '*' || c == '/'; // TODO consts
 }
 
 int isRightParenthesis(struct Input input)
 {
-    char c;
-    if (!isChar(input))
-    {
-        return 0;
-    }
-    c = (char) input.value;
-    return c == '(';
+	char c;
+	if (!isChar(input))
+	{
+		return 0;
+	}
+	c = (char) input.value;
+	return c == '(';
 }
 
 int isLeftParenthesis(struct Input input)
 {
-    char c;
-    if (!isChar(input))
-    {
-        return 0;
-    }
-    c = (char) input.value;
-    return c == ')';
+	char c;
+	if (!isChar(input))
+	{
+		return 0;
+	}
+	c = (char) input.value;
+	return c == ')';
 }
 
 int isOperand(struct Input input)
 {
-    return input.type == NUMBER_TYPE;
+	return input.type == NUMBER_TYPE;
 }
 
 int isSpace(char c)
 {
-    return c == ' ';
+	return c == ' ';
 }
 
 void printInputs(struct Input* inputs, int inputsSize)
 {
-    int i;
-    struct Input curInput;
-    for (i = 0; i < inputsSize; i++) {
-        curInput = inputs[i];
-        if (isOperand(curInput)) {
-            printf(" %d", curInput.value);
-        } else {
-            printf("%c", curInput.value);
-        }
-    }
-    printf("\n");
+	int i;
+	struct Input curInput;
+	for (i = 0; i < inputsSize; i++) {
+		curInput = inputs[i];
+		if (isOperand(curInput)) {
+			printf(" %d", curInput.value);
+		} else {
+			printf("%c", curInput.value);
+		}
+	}
+	printf("\n");
 }
